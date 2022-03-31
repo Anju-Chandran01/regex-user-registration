77c86bae9f027937badab9152092b36b434f35ad
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-03-31 09:39:53 -0700

    Added : As a User enter a valid Last Name

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -7,11 +7,21 @@ public class UserRegistration {
     public static void main(String[] args) {
 
         final String FIRST_NAME = "^[A-Z][a-z]{2}[a-z]*";
+        final String LAST_NAME = "^[A-Z][a-z]{2}[a-z]*";
 
-        String firstName = "Anju";
+        String firstName = "Amaya";
+        String lastName = "Prasanth";
 
+        // First Name validation
         if (Pattern.matches(FIRST_NAME, firstName)) {
             System.out.println("Valid first name");
+        } else {
+            System.out.println("Invalid");
+        }
+
+        //Last Name Validation
+        if (Pattern.matches(FIRST_NAME, firstName)) {
+            System.out.println("Valid last name");
         } else {
             System.out.println("Invalid");
         }
